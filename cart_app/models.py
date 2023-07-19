d61fdb09c9a8c1944ed74df74e63695f3e515a4b
fravila08 <fr4v1l4@gmail.com>
2023-07-19 13:53:58 -0700

    template

--- cart_app/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class Cart(models.Model):
-    pass
-
-class Cart_item(models.Model):
-    pass
--- cart_app/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-from rest_framework.views import APIView
-# Create your views here.
-
-class Cart_manager(APIView):
-    pass
