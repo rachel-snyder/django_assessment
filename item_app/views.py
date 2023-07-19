2fe5b2c387c11c0180e123bb90e3e0373e5d4946
fravila08 <fr4v1l4@gmail.com>
2023-07-19 09:30:35 -0700

    init template

---
+++ item_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+# Create your views here.
+class All_items(APIView):
+    pass
+
+class An_item(APIView):
+    pass
+
+class Item_by_category(APIView):
+    pass
---
+++ user_app/views.py
@@ -0,0 +1,12 @@
+from django.shortcuts import render
+from rest_framework.views import APIView
+
+# Create your views here.
+class Sign_Up(APIView):
+    pass
+
+class Log_in(APIView):
+    pass
+
+class Log_out(APIView):
+    pass
